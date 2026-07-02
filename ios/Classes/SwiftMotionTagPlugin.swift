4a27c88552cbce38af4272fd97b0b0ce4f1a647e
kianm <keyanm@gmail.com>
2026-07-02 14:55:37 +0200

    update flutter call interface

--- ios/Classes/SwiftMotionTagPlugin.swift
+++ ios/Classes/SwiftMotionTagPlugin.swift
@@ -1,98 +1,99 @@
 import Flutter
 import UIKit
+import CoreLocation
 import MotionTagSDK
 
-// TODO: Add unit tests
 public class SwiftMotionTagPlugin: NSObject, FlutterPlugin {
 
-    private lazy var motionTag = MotionTagCore.sharedInstance
     private var channel: FlutterMethodChannel
 
     init(channel: FlutterMethodChannel) {
         self.channel = channel
-	super.init()
+        super.init()
     }
 
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "de.motiontag.tracker", binaryMessenger: registrar.messenger())
-        let instance = SwiftMotionTagPlugin(channel: channel);
-        MotionTagDelegateWrapper.sharedInstance.channel = channel
+        let instance = SwiftMotionTagPlugin(channel: channel)
+        MainActor.assumeIsolated {
+            MotionTagDelegateWrapper.sharedInstance.channel = channel
+        }
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch (call.method) {
-        case "getUserToken":
-            let userToken = motionTag.userToken
-            result(userToken)
-        case "setUserToken":
-            let args = call.arguments as! Dictionary<String, Any>
-            motionTag.userToken = args["userToken"] as! String
-            result(nil)
-        case "getWifiOnlyDataTransfer":
-            let wifiOnlyDataTransfer = motionTag.wifiOnlyDataTransfer
-            result(wifiOnlyDataTransfer)
-        case "setWifiOnlyDataTransfer":
-            let args = call.arguments as! Dictionary<String, Any>
-            motionTag.wifiOnlyDataTransfer = args["wifiOnlyDataTransfer"] as! Bool
-            result(nil)
-        case "start":
-            motionTag.start()
-            result(nil)
-        case "stop":
-            motionTag.stop()
-            result(nil)
-        case "clearData":
-            motionTag.clearData()
-            result(nil)
-        case "isTrackingActive":
-            let isTrackingActive = motionTag.isTrackingActive
-            result(isTrackingActive)
-        default:
-            let error = FlutterError(code: "UNKNOWN_METHOD", message: "Unknown method \(call.method)", details: nil)
-            result(error)
+        MainActor.assumeIsolated {
+            var motionTag = MotionTagCore.sharedInstance
+            switch call.method {
+            case "getUserToken":
+                result(motionTag.userToken)
+            case "setUserToken":
+                let args = call.arguments as! Dictionary<String, Any>
+                motionTag.userToken = args["userToken"] as! String
+                result(nil)
+            case "getWifiOnlyDataTransfer":
+                result(motionTag.wifiOnlyDataTransfer)
+            case "setWifiOnlyDataTransfer":
+                let args = call.arguments as! Dictionary<String, Any>
+                motionTag.wifiOnlyDataTransfer = args["wifiOnlyDataTransfer"] as! Bool
+                result(nil)
+            case "start":
+                motionTag.start()
+                result(nil)
+            case "stop":
+                motionTag.stop()
+                result(nil)
+            case "clearData":
+                motionTag.clearData()
+                result(nil)
+            case "isTrackingActive":
+                result(motionTag.isTrackingActive)
+            default:
+                result(FlutterError(code: "UNKNOWN_METHOD", message: "Unknown method \(call.method)", details: nil))
+            }
         }
     }
 }
 
+@MainActor
 public class MotionTagDelegateWrapper: NSObject, MotionTagDelegate {
 
     public static let sharedInstance = MotionTagDelegateWrapper()
     public weak var channel: FlutterMethodChannel? = nil
 
-    private override init() {
-    }
+    private override init() {}
 
-    public func trackingStatusChanged(_ isTracking: Bool) {
+    public func trackingDidChange(isTracking: Bool) {
         let arguments = isTracking ? ["type": "STARTED"] : ["type": "STOPPED"]
         didEventOccur(arguments)
     }
 
-    public func locationAuthorizationStatusDidChange(_ status: CLAuthorizationStatus, precise: Bool) {
+    public func locationAuthorizationDidChange(status: CLAuthorizationStatus, isPrecise: Bool) {
         // Ignored
     }
 
-    public func motionActivityAuthorized(_ authorized: Bool) {
+    public func motionActivityAuthorizationDidChange(isAuthorized: Bool) {
         // Ignored
     }
 
-    public func didTrackLocation(_ location: CLLocation) {
-        let arguments: [String: Any] = ["type": "LOCATION",
-                                        "timestamp": location.timestamp.timestampMs,
-                                        "latitude": location.coordinate.latitude,
-                                        "longitude": location.coordinate.longitude,
-                                        "horizontalAccuracy": location.horizontalAccuracy,
-                                        "speed": location.speed < 0 ? nil : location.speed,
-                                        "altitude": location.verticalAccuracy <= 0 ? nil : location.altitude,
-                                        "bearing": location.course < 0 ? nil : location.course]
+    public func didUpdateLocation(_ location: CLLocation) {
+        var arguments: [String: Any] = [
+            "type": "LOCATION",
+            "timestamp": location.timestamp.timestampMs,
+            "latitude": location.coordinate.latitude,
+            "longitude": location.coordinate.longitude,
+            "horizontalAccuracy": location.horizontalAccuracy
+        ]
+        if location.speed >= 0 { arguments["speed"] = location.speed }
+        if location.verticalAccuracy > 0 { arguments["altitude"] = location.altitude }
+        if location.course >= 0 { arguments["bearing"] = location.course }
         didEventOccur(arguments)
     }
 
-    public func dataUploadWithTracked(from startDate: Date, to endDate: Date, didCompleteWithError error: Error?) {
+    public func dataUploadDidComplete(from startDate: Date, to endDate: Date, error: Error?) {
         var arguments: [String: Any]
         if let error = error {
-            arguments = ["type": "TRANSMISSION_ERROR",
-                         "error": error.localizedDescription]
+            arguments = ["type": "TRANSMISSION_ERROR", "error": error.localizedDescription]
         } else {
             arguments = ["type": "TRANSMISSION_SUCCESS",
                          "trackedFrom": startDate.timestampMs,
@@ -107,7 +108,6 @@ public class MotionTagDelegateWrapper: NSObject, MotionTagDelegate {
 }
 
 private extension Date {
-
     var timestampMs: Int {
         Int(self.timeIntervalSince1970 * 1_000)
     }
